098ce225a6ce022d32620c192865c17f5bae114d
Stefenatefun <stefenatefun@gmail.com>
2014-09-19 11:31:16 -0400

    Initial commit

---
+++ src/main/java/com/stefensharkey/pvplogger/PvPLogger.java
@@ -0,0 +1,18 @@
+package com.stefensharkey.pvplogger;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PvPLogger extends JavaPlugin
+{
+    @Override
+    public void onEnable()
+    {
+
+    }
+
+    @Override
+    public void onDisable()
+    {
+
+    }
+}
