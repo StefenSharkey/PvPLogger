daaed57b791cedc394459080107ada8d1c476a9b
Stefenatefun <stefenatefun@gmail.com>
2014-09-19 12:04:07 -0400

    Clean up code

--- src/main/java/com/stefensharkey/pvplogger/PvPLogger.java
+++ src/main/java/com/stefensharkey/pvplogger/PvPLogger.java
@@ -2,12 +2,16 @@ package com.stefensharkey.pvplogger;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Logger;
+
 public class PvPLogger extends JavaPlugin
 {
+    private Logger log;
+
     @Override
     public void onEnable()
     {
-
+        log = getLogger();
     }
 
     @Override
