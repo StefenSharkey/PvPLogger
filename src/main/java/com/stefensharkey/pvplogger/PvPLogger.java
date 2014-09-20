f7088091255d8e9652449b506d8a1fcb58166773
Stefenatefun <stefenatefun@gmail.com>
2014-09-20 00:12:34 -0400

    Add extra logging features

--- src/main/java/com/stefensharkey/pvplogger/PvPLogger.java
+++ src/main/java/com/stefensharkey/pvplogger/PvPLogger.java
@@ -1,51 +1,18 @@
 package com.stefensharkey.pvplogger;
 
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.logging.Logger;
 
 public class PvPLogger extends JavaPlugin
 {
-    private Logger log;
-
     @Override
     public void onEnable()
     {
-        log = getLogger();
+        getServer().getPluginManager().registerEvents(new PvPLoggerListener(this), this);
     }
 
     @Override
     public void onDisable()
     {
-
-    }
-
-    public void logToFile(Entity attacker, Entity damaged, String message)
-    {
-        File dataFolder = getDataFolder();
-        File attackerFile;
-        File damagedFile;
-
-        if(attacker instanceof Player)
-        {
-            attackerFile = new File(dataFolder, ((Player)attacker).getUniqueId().toString());
-            if(!attackerFile.exists())
-                try
-                {
-                    attackerFile.createNewFile();
-                } catch(IOException e)
-                {
-                    e.printStackTrace();
-                }
-        }
-
-        if(!dataFolder.exists())
-            dataFolder.mkdir();
-
 
     }
 }
--- src/main/java/com/stefensharkey/pvplogger/PvPLoggerListener.java
+++ src/main/java/com/stefensharkey/pvplogger/PvPLoggerListener.java
@@ -1,7 +1,7 @@
 package com.stefensharkey.pvplogger;
 
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
@@ -10,22 +10,105 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.plugin.Plugin;
 
-public class PvPLoggerListener implements Listener
-{
-    private Plugin instance;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
-    public PvPLoggerListener(Plugin instance)
+public final class PvPLoggerListener implements Listener
+{
+    private Plugin plugin;
+
+    public PvPLoggerListener(Plugin plugin)
     {
-        this.instance = instance;
-        Bukkit.getServer().getPluginManager().registerEvents(this, instance);
+        this.plugin = plugin;
     }
 
     @EventHandler(priority = EventPriority.LOWEST)
+    @SuppressWarnings("unused")
     public void onEntityDamageByEntityEvent(final EntityDamageByEntityEvent event)
     {
-        if(event.getDamager() instanceof Player)
-        {
+        logToFile(event.getDamager(), event.getEntity(), formatMessage(event));
+    }
 
+    public String entityInfo(Entity entity)
+    {
+        return formatName(entity)
+                + " {Coordinates={X=" + entity.getLocation().getBlockX()
+                + " Y=" + entity.getLocation().getBlockY()
+                + " Z=" + entity.getLocation().getBlockZ()
+                + "}, Orientation={Yaw=" + entity.getLocation().getYaw()
+                + " Pitch=" + entity.getLocation().getPitch()
+                + "}, World=" + entity.getLocation().getWorld().getName() + "}";
+    }
+
+    public String formatMessage(EntityDamageByEntityEvent event)
+    {
+        Calendar cal = Calendar.getInstance();
+        cal.getTime();
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+        return "[" + sdf.format(cal.getTime()) + "]: " + formatName(event.getDamager()) + " (UUID: "
+                + event.getDamager().getUniqueId() + ") attacked " + formatName(event.getEntity()) + " (UUID: "
+                + event.getEntity().getUniqueId() + ") with " + formatWeapon(event.getDamager()) + " for "
+                + event.getDamage() + " damage."
+                + "\n" + entityInfo(event.getDamager()) + "\n" + entityInfo(event.getEntity())
+                + "\n";
+    }
+
+    public String formatName(Entity entity)
+    {
+        if(entity instanceof Projectile)
+            return ((Projectile) entity).getShooter().toString();
+        if(entity instanceof Player)
+            return ((Player) entity).getName();
+        return entity.toString();
+    }
+
+    public String formatWeapon(Entity entity)
+    {
+        if(entity instanceof LivingEntity)
+            return ((LivingEntity) entity).getEquipment().getItemInHand().toString();
+        return entity.toString();
+    }
+
+    public void logToFile(Entity attacker, Entity damaged, String message)
+    {
+        File saveTo;
+        FileWriter fileWriter;
+        PrintWriter printWriter;
+
+        if(!plugin.getDataFolder().exists())
+            plugin.getDataFolder().mkdir();
+
+        try
+        {
+            if(attacker instanceof Player)
+            {
+                saveTo = new File(plugin.getDataFolder(), (attacker).getUniqueId().toString() + ".log");
+
+                fileWriter = new FileWriter(saveTo, true);
+                printWriter = new PrintWriter(fileWriter);
+                printWriter.println(message);
+                printWriter.flush();
+                printWriter.close();
+            }
+
+            if(damaged instanceof Player)
+            {
+                saveTo = new File(plugin.getDataFolder(), (damaged).getUniqueId().toString() + ".log");
+
+                fileWriter = new FileWriter(saveTo, true);
+                printWriter = new PrintWriter(fileWriter);
+                printWriter.println(message);
+                printWriter.flush();
+                printWriter.close();
+            }
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
         }
     }
 }
